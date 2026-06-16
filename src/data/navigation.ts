628825b69366e7cde2bf6bd24f419278afddadb4
Patrick Perkins <pperkins@upenn.edu>
2026-06-16 12:17:38 -0400

    about becomes index

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -1,6 +1,5 @@
 export const navLinks = [
   { href: '/blog', label: 'Blog' },
   { href: '/projects', label: 'Projects' },
-  { href: '/about', label: 'About' },
   { href: '/contact', label: 'Contact' },
 ];
